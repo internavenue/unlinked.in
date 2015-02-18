452bc88bd2b70350c327a18408a41d7648f040f6
nvcnvn <nguyen@open-vn.org>
2015-02-18 18:13:02 +0700

    restructure app for easy testing

--- main.go
+++ main.go
@@ -3,45 +3,18 @@ package main
 import (
 	"flag"
 	log "github.com/Sirupsen/logrus"
-	"github.com/gorilla/mux"
-	"github.com/gorilla/sessions"
-	"html/template"
+	"github.com/internavenue/unlinked.in/server"
 	"net/http"
-	"path/filepath"
 )
 
 func main() {
 	var cfgfile string
 	flag.StringVar(&cfgfile, "cfgfile", "./cfg.json", "path to JSON config file")
 
-	config, err := ReadConfig(cfgfile)
+	config, err := server.ReadConfig(cfgfile)
 	if err != nil {
 		log.Fatal("Cannot load config from file", cfgfile, err)
 	}
 
-	tmpl := template.Must(template.
-		ParseGlob(filepath.Join(config.TemplateDir, "*.tmpl")))
-	store := sessions.NewCookieStore([]byte(config.SessionKey))
-
-	r := mux.NewRouter()
-	r.Handle("/auth/redirect", &Server{
-		store:   store,
-		config:  config,
-		handler: ResirectHandler,
-	})
-
-	r.Handle("/export", &Server{
-		store:   store,
-		config:  config,
-		handler: ProfileExportHandler,
-	})
-
-	r.Handle("/", &Server{
-		store:    store,
-		config:   config,
-		template: tmpl,
-		handler:  IndexHandler,
-	})
-
-	log.Info(http.ListenAndServe(config.HTTPAddress, r))
+	log.Info(http.ListenAndServe(config.HTTPAddress, server.NewHandler(config)))
 }
